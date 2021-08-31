dc482a7c97564553836c89418ef639fa30758a8f
Siddharth Teli <telisiddharth123@gmail.com>
2021-08-31 16:44:07 +0530

    Add:Jsx variable to Jsx . Implemented:
    Because:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const name_app="Bada TOdo";
+  const title_app="Todo Application";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <h1>
+       {name_app}
+     </h1>
+     <h1>
+       {title_app}
+     </h1>
     </div>
   );
 }
