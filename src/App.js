60b210768553f07ae42d5b7abf664a05dff50cf4
Siddharth Teli <telisiddharth123@gmail.com>
2021-08-31 18:45:39 +0530

    Add:Class impl of App in App.js Implemented:
    Because:

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./componenets/Header"
-function App() {
-  const name_app="Bada TOdo";
-  const title_app="Todo Application";
-  return (
-    <div className="container">
-      <Header/>
-     <h1>
-       {name_app}
-     </h1>
-     <h1>
-       {title_app}
-     </h1>
-    </div>
-  );
-}
 
-export default App;
+// import logo from "./logo.svg";
+// import "./App.css";
+// import Header from "./componenets/Header"
+// function App() {
+//   const name_app="Bada TOdo";
+//   const title_app="Todo Application";
+//   return (
+//     <div className="container">
+//       <Header/>
+//      <h1>
+//        {name_app}
+//      </h1>
+//      <h1>
+//        {title_app}
+//      </h1>
+//     </div>
+//   );
+// }
+//export default App;
+
+//class based impl -
+
+import React from "react";
+
+class App extends React.Component{
+  render() {
+    return <h1>Hello this is good</h1>
+  }
+}
+export default App
