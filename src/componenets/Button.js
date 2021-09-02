9be2674a3e692fe689fc6cd88876f05e07659ca9
Siddharth Teli <telisiddharth123@gmail.com>
2021-09-03 01:11:22 +0530

    Add:State to update button text. Implemented:
    Because:

--- src/componenets/Button.js
+++ src/componenets/Button.js
@@ -1,11 +1,12 @@
 //This is just a blueprint of button.
 //We can include mutiple of this button .
-
+import { useState } from "react";
 import  PropTypes from "prop-types";
 const Button = ({onAdd}) => {
+    const [message,Po]=useState(40);
 
     return(
-    <button onClick={onAdd}>Save</button>)
+    <button onClick={() =>Po(message+5)}>{message}</button>)
 };
 
 
